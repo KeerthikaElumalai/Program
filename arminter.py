d1833258a4576a06c9ff5231ffdba277e13c37c0
KeerthikaElumalai <46592686+KeerthikaElumalai@users.noreply.github.com>
2019-01-31 21:33:23 +0530

    Update arminter.py

--- arminter.py
+++ arminter.py
@@ -1,9 +1,11 @@
-for num in range(100,1000):
-  temp=num
-  sum=0
-  while temp>0:
-      digit=temp%10
-      sum=sum+digit**3
-      temp=temp//10
-      if sum==num:
-           print (num)
+lower = int(input("Enter lower range: "))
+upper = int(input("Enter upper range: "))
+ for num in range(lower,upper + 1):
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+    if num == sum:
+       print(num)
