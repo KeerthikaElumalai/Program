40b4e2a817bf248ffc2265b4ea538cb8e24ea9ec
KeerthikaElumalai <46592686+KeerthikaElumalai@users.noreply.github.com>
2019-02-01 09:56:42 +0530

    Create greater.c

---
+++ greater.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int num1, num2, num3;
+ 
+    printf("\n");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    printf("%d\t %d\t %d\n", num1, num2, num3);
+    if (num1 > num2)
+    {
+        if (num1 > num3)
+        {
+            printf("%d\n",num1);
+        }
+        else
+        {
+            printf("%d \n",num3);
+        }
+    }
+    else if (num2 > num3)
+        printf("%d \n",num2);
+    else
+        printf("%d \n",num3);
+
+}
