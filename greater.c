f8aa0721ef88b0fafcc7c0038172f2692e192c9f
KeerthikaElumalai <46592686+KeerthikaElumalai@users.noreply.github.com>
2019-02-01 09:59:16 +0530

    Update greater.c

--- greater.c
+++ greater.c
@@ -6,7 +6,7 @@ int main()
  
     printf("\n");
     scanf("%d %d %d", &num1, &num2, &num3);
-    printf("%d\t %d\t %d\n", num1, num2, num3);
+    //printf("%d\t %d\t %d\n", num1, num2, num3);
     if (num1 > num2)
     {
         if (num1 > num3)
