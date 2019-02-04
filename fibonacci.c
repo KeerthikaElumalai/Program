fa489f6573922dab0b247b8ad18cb58eb1ed61ea
KeerthikaElumalai <46592686+KeerthikaElumalai@users.noreply.github.com>
2019-02-04 00:55:11 -0800

    Update and rename fibonacci.py to fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+         int main(void)
+         {
+         int i,n,t1=1,t2=1, t3;
+         scanf("%d",&n);
+               for(i=1;i<=n;++i)
+               {
+               printf("%d\t",t1);
+               t3=t1+t2;
+               t1=t2;
+               t2=t3;
+               	
+               }
+               return 0;
+               
+         }
+
