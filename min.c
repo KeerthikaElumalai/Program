12c3c6bab83b60af04cf8b6c64ece263419b427d
KeerthikaElumalai <46592686+KeerthikaElumalai@users.noreply.github.com>
2019-02-04 00:35:58 -0800

    Update min.c

--- min.c
+++ min.c
@@ -6,13 +6,13 @@ int main ( void )
 	printf("");
 	scanf("%0ld",&numMinutes);
 if ( (numMinutes / 60) == 1 ) 
-  printf("%01d hour",numMinutes/60);
+  printf("%01d",numMinutes/60);
 else 
-  printf("%01d hours",numMinutes/60);
+  printf("%01d",numMinutes/60);
 
 if ( (numMinutes % 60) == 1) 
-  printf(" %01d minute\n",numMinutes%60);
+  printf(" %01d\n",numMinutes%60);
 else 
-  printf(" %01d minutes\n",numMinutes%60);
+  printf(" %01d\n",numMinutes%60);
 return 0;
 }
