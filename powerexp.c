d3867e4c371283e6a849b6694d4cc4d2b6dc7e76
KeerthikaElumalai <46592686+KeerthikaElumalai@users.noreply.github.com>
2019-02-05 20:57:45 +0530

    Create powerexp.c

---
+++ powerexp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+int base,exponent;
+long long result = 1;
+scanf("%d%d", &base,&exponent);
+while (exponent != 0)
+{
+   result *= base;
+   --exponent;
+}
+printf("%lld", result);
+return 0;
+}
