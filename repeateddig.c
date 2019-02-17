aa32cbbe2b70e684e669cfbb3c82daf071530188
KeerthikaElumalai <46592686+KeerthikaElumalai@users.noreply.github.com>
2019-02-17 15:00:04 +0530

    Create repeateddig.c

---
+++ repeateddig.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(void)
+{
+ int num,k,temp,frequency[9],flag=0,i;
+ scanf("%d",&num);
+ temp=num;
+ for(i=0;i<10;i++)
+ {
+     frequency[i]=0;
+ }
+ while(num>0)
+ {
+  k=num%10;
+  frequency[k]++;
+  num/=10;  
+ }
+ for(i=0;i<10;i++)
+ {
+  if(frequency[i]>1)
+  {
+   flag=1;
+   printf("\n",i,frequency[i]);
+  }
+ }
+ if(flag==0)
+ {
+  printf("no\n");
+ }
+ else
+ {
+  printf("yes\n");
+ }
+}
