38e328d6fe7d79f3ac9219e3c4c8f8528317d8fe
KeerthikaElumalai <46592686+KeerthikaElumalai@users.noreply.github.com>
2019-02-20 21:21:58 +0530

    Create ascen.c

---
+++ ascen.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(void)
+{
+ int i, j, a, n, num[30];
+  scanf("%d", &n);
+ for (i = 0; i < n; ++i)
+  scanf("%d", &num[i]);
+ for (i = 0; i < n; ++i)
+  {
+   for (j = i + 1; j < n; ++j)
+   {
+   if (num[i] > num[j])
+    {
+     a = num[i];
+     num[i] = num[j];
+     num[j] = a;
+     }
+    }
+   }
+   for(i = 0; i < n; ++i)
+    printf("%d", num[i]);
+  }
