05d1d2e10b3c650559ed9737b25316d699859f1f
KeerthikaElumalai <46592686+KeerthikaElumalai@users.noreply.github.com>
2019-06-18 21:23:04 +0530

    Update pro45.py

--- pro45.py
+++ pro45.py
@@ -2,12 +2,12 @@ a=input()
 if a==a[::-1]:
     print("yes")
 else:
-    vin=n.strip("0")
-        if vin==vin[::-1]:
+    value=n.strip("0")
+        if value==value[::-1]:
         print("yes")
     else:
-        vin=a.lstrip("0")
-        if vin==vin[::-1]:
+        value=a.lstrip("0")
+        if value==value[::-1]:
             print("yes")
         else:
             print("no")
